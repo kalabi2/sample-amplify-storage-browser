69f80e3ef521ab3d9c4a88bcc5b4ceeed6a72bbf
kalabi <kalabi@deloitte.ie>
2025-05-21 12:35:06 +0100

    existing bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,36 +1,12 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myStorageBucket',
+  name: 'dedicated-switch-bucket',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
+    'switch-folder/*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
-});
-
-export const secondaryStorage = defineStorage({
-  name: 'mySecondaryStorageBucket',
-   access: (allow) => ({
-    'backup_public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
